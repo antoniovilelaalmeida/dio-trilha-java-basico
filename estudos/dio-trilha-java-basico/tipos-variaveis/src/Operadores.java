c28cddd071caf2ba72142e8ed237839cd171000c
Antonio Neto <91612971+antoniovilelaalmeida@users.noreply.github.com>
2023-03-30 14:44:26 -0300

    Add files via upload

---
+++ estudos/dio-trilha-java-basico/tipos-variaveis/src/Operadores.java
@@ -0,0 +1,11 @@
+import javax.sound.midi.Soundbank;
+
+public class Operadores {
+
+    public static void main(String[]args){
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+    
+        int numero = 5;
+
+    }
+}
